9838f0bca7e2de6a28b4f5103e492ef095e7eccc
Harshita Nidhi <harshitanidhi17@gmail.com>
2021-10-16 19:43:00 +0530

    java script basics 2

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ console.log("hello world")
 var heading = document.getElementById("heading")
 console.log(heading)
 console.log(typeof heading)
-heading.innerHTML = "Hi from JS"
+heading.innerHTML = "Hi from JS"   // will change the element in the tag
+heading.style = "color:blue;font-size:2rem"   // style 
 console.log(heading)
 
 var para = document.getElementsByClassName("para")
@@ -17,6 +18,12 @@ console.log(para[1])
 var tags = document.getElementsByTagName("p")
 console.log(tags)
 
-var q = document.querySelector("#heading")
-var q = document.querySelectorAll(".para")
-console.log(q)
+var qi = document.querySelector("#heading")
+console.log(qi)
+var qc = document.querySelector(".para")
+// var qc = document.querySelectorAll(".para")
+console.log(qc)
+
+console.log("class List", para3.classList)
+heading.classList.add('four')
+heading.classList.remove('two')
