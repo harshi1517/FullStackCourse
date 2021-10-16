a2bae335ae1c051abff55a1a1974d72275cb1f8b
Harshita Nidhi <harshitanidhi17@gmail.com>
2021-10-16 20:23:16 +0530

    dark and light mode

---
+++ app2.js
@@ -0,0 +1,9 @@
+console.log("HYE")
+
+var para = document.getElementById("para")
+console.log(para)
+
+
+var body = document.querySelector('body')
+body.classList.add('dark')
+body.classList.remove('dark')
