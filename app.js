72b1fa28b47fcc6afee24e3e8833a393d623d625
Harshita Nidhi <harshitanidhi17@gmail.com>
2021-10-18 00:27:25 +0530

    simple calculator

--- app.js
+++ app.js
@@ -1,83 +1,39 @@
-//FUNCTIONS
-function greet(Name, age){
-    console.log("HELLO!")
-    console.log(Name, age)
-}
-greet("Harshita", 20)
 
-function greet(Name, age){
-    console.log("HELLO! " + Name)
-    console.log(age)   //in place of age it will show undefined
-}
-greet("Harshita")
-
-
-function sum(a, b){
-    console.log("a+b=", a+b)
-}
-sum(15,17)
-
-function sum(a, b){
-    console.log("a+b=", a+b)
-}
-sum(15)
-
-
-function add(a, b){
-    return  a+b
-}
-console.log("a+b=", add(18, 2))
+// const input1 = document.getElementsByTagName('input')[0]
+// const input2 = document.getElementsByTagName('input')[1]
 //or
-function add(a, b){
-    var sum = a+b
-    return  sum
+const a = document.querySelectorAll('input')[0]
+const b = document.querySelectorAll('input')[1]
+console.log(a, b)
+
+const add = document.querySelector('#add')
+const sub = document.querySelector('#sub')
+const multi = document.querySelector('#multi')
+const div = document.querySelector('#div')
+console.log(add, sub, multi,div)
+
+const result = document.querySelector('.result')
+console.log(result)
+
+const sum = () => {
+    const ans = parseInt(a.value) + parseInt(b.value)      //a.value act like a string so to change it to int we use parseInt 
+    result.innerHTML = ans              //prints ans on veiwport
+    // result.textContent = ans
 }
-var result = add(5, 7)
-console.log("a+b=", result)
-
-//above are function keyword declaration(normal declaration) and
-// in below one we change it to a function expression
-var add = function(a=0, b=0){
-    return a+b
+const substract = () => {
+    const ans = parseInt(a.value) - parseInt(b.value)     
+    result.innerHTML = ans           
 }
-console.log("a+b=", add(50, 20))  //if this console.log will be placed above this function then it will throw an error
-
-//ARROW FUNCTION
-var add = (a=0, b=0) => {
-    return a+b
+const multiply = () => {
+    const ans = parseInt(a.value) * parseInt(b.value)     
+    result.innerHTML = ans           
 }
-//or
-//var add = (a, b) => a+b
-console.log("a+b=", add(50, 20))
-
-var greet = (Name, age) => {
-    console.log("HELLO!")
-    console.log(Name, age)
+const divide = () => {
+    const ans = parseInt(a.value) / parseInt(b.value)     
+    result.innerHTML = ans           
 }
-greet("Harshita", 20)
-//or
-var greet = Name => 'hi '+ Name
-console.log(greet("Harshita"))
 
-//CALL BACK FUNCTION
-var arr = ['study', 'workout', 'project']
-// for(var i = 0; i < arr.length; i++)
-// {
-//     console.log(arr[i])
-// }
-var callbackFunction = () => {
-    console.log('assignment')}
-arr.forEach(callbackFunction)
-
-var callbackFunction = (element) => {
-    console.log(element)}
-arr.forEach(callbackFunction)
-
-var callbackFunction = (element, index) => {
-    console.log(element, index)}
-arr.forEach(callbackFunction)
-//or
-arr.forEach((element, index) => {
-    console.log(element, index)
-})
-
+add.addEventListener('click', sum)
+sub.addEventListener('click', substract)
+multi.addEventListener('click', multiply)
+div.addEventListener('click', divide)
