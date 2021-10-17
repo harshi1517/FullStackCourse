1b1d4adf48978bae378da299a9ef2b4c3d488a21
Harshita Nidhi <harshitanidhi17@gmail.com>
2021-10-17 09:32:02 +0530

    functions in js

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
-//document.getElementsByClassName //document is an object,  getElementsByClassName is a method
+//functions
+function greet(Name, age){
+    console.log("HELLO!")
+    console.log(Name, age)
+}
+greet("Harshita", 20)
 
-console.log("hello world")
+function greet(Name, age){
+    console.log("HELLO! " + Name)
+    console.log(age)   //in place of age it will show undefined
+}
+greet("Harshita")
 
-var heading = document.getElementById("heading")
-console.log(heading)
-console.log(typeof heading)
-heading.innerHTML = "Hi from JS"   // will change the element in the tag
-heading.style = "color:blue;font-size:2rem"   // style 
-console.log(heading)
+function sum(a, b){
+    console.log("a+b=", a+b)
+}
+sum(15,17)
 
-var para = document.getElementsByClassName("para")
-console.log(para)
-console.log(para.length)
-console.log(para[0])
-console.log(para[1])
+function sum(a, b){
+    console.log("a+b=", a+b)
+}
+sum(15)
 
-var tags = document.getElementsByTagName("p")
-console.log(tags)
+function add(a, b){
+    return  a+b
+}
+console.log("a+b=", add(18, 2))
 
-var qi = document.querySelector("#heading")
-console.log(qi)
-var qc = document.querySelector(".para")
-// var qc = document.querySelectorAll(".para")
-console.log(qc)
-
-console.log("class List", para3.classList)
-heading.classList.add('four')
-heading.classList.remove('two')
-console.log("class List", para3.classList)
+function add(a, b){
+    var sum = a+b
+    return  sum
+}
+var result = add(5, 7)
+console.log("a+b=", result)
