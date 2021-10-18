60fda18a3489d5d4f48089e4822995b3487ddba9
Harshita Nidhi <harshitanidhi17@gmail.com>
2021-10-18 10:35:04 +0530

    Array Methods

--- app.js
+++ app.js
@@ -1,95 +1,41 @@
+// array method
 
-// const input1 = document.getElementsByTagName('input')[0]
-// const input2 = document.getElementsByTagName('input')[1]
-//or
-const a = document.querySelectorAll('input')[0]
-const b = document.querySelectorAll('input')[1]
-// console.log(a, b)
+var arr = [1,2,3,4,5,6]
+//arr.push(5)      //add the element in end
+arr.unshift(5)      //add the element in begining
+console.log(arr)
+// console.log(arr.push(6))
 
-const add = document.querySelector('#add')
-const sub = document.querySelector('#sub')
-const multi = document.querySelector('#multi')
-const div = document.querySelector('#div')
-const power = document.querySelector('#power')
-// console.log(add, sub, multi,div)
+// arr.shift()         //removes the first element
 
-const result = document.querySelector('.result')
-// console.log(result)
+var new_arr = arr.slice(2, 4)     //arr.slice will not change the original array
+console.log(new_arr)             //it will returt a new array starting with index 2(included) till index 4(excluded)
 
-// const sum = () => {
-//     const ans = parseInt(a.value) + parseInt(b.value)      //a.value act like a string so to change it to int we use parseInt 
-//     result.innerHTML = ans              //prints ans on veiwport
-//     // result.textContent = ans
-// }
-// const substract = () => {
-//     const ans = parseInt(a.value) - parseInt(b.value)     
-//     result.innerHTML = ans           
-// }
-// const multiply = () => {
-//     const ans = parseInt(a.value) * parseInt(b.value)     
-//     result.innerHTML = ans           
-// }
-// const divide = () => {
-//     const ans = parseInt(a.value) / parseInt(b.value)     
-//     result.innerHTML = ans           
-// }
-// const power = () => {
-    //     const ans = parseInt(a.value) ** parseInt(b.value)     
-    //     result.innerHTML = ans           
-    // }
+arr.splice(5, 1)             //it will remove the element at index 3, and the 2nd parameter is num of element to remove     
+console.log(arr)
 
-// add.addEventListener('click', sum)
-// sub.addEventListener('click', substract)
-// multi.addEventListener('click', multiply)
-// div.addEventListener('click', divide)
-// power.addEventListener('click', power)
 
-//or
-
-const calculate = (event, operation) => {
-
-    // if (operation == 'add')
-    // result.innerHTML = parseInt(a.value) + parseInt(b.value)     
-    
-    // else if (operation == 'sub')
-    // result.innerHTML = parseInt(a.value) - parseInt(b.value)     
-
-    // else if (operation == 'multi')
-    // result.innerHTML = parseInt(a.value) * parseInt(b.value)     
-
-    // else if (operation == 'div')
-    // result.innerHTML = parseInt(a.value) / parseInt(b.value)     
-
-    // else if (operation == 'power')
-    // result.innerHTML = parseInt(a.value) ** parseInt(b.value)
-    
-    // else
-    // result.innerHTML = 'Not a Valid Operation'
-
-    //using switch case
-    switch(operation){
-     case 'add':
-        result.innerHTML = parseInt(a.value) + parseInt(b.value)
-        break
-     case 'sub':
-        result.innerHTML = parseInt(a.value) - parseInt(b.value)
-        break
-     case 'multi':
-        result.innerHTML = parseInt(a.value) * parseInt(b.value)
-        break
-     case 'div':
-        result.innerHTML = parseInt(a.value) / parseInt(b.value)
-        break
-     case 'power':
-        result.innerHTML = parseInt(a.value) ** parseInt(b.value)
-        break
-     default:
-        result.innerHTML = 'Not a Valid Operation'
-    }
+//store even num in arr2
+var arr2 = []
+for (var i = 0;  i < arr.length; i++) {
+    if(arr[i] % 2 == 0)
+    arr2.push(arr[i]) 
 }
-add.addEventListener('click', (event) =>  calculate(event, 'add'))
-sub.addEventListener('click', (event) =>  calculate(event, 'sub'))
-multi.addEventListener('click', (event) =>  calculate(event, 'multi'))
-div.addEventListener('click', (event) =>  calculate(event, 'div'))
-power.addEventListener('click', (event) =>  calculate(event, 'power'))
+console.log(arr2)
+
+//for these operation there is a filter method in array
+arr.filter((element, index) => {
+    console.log(element, index)    //will return all elements of array with index
+})
+
+var arr3 = arr.filter((element) => {
+    if(element % 2 == 0) return true
+})
+console.log(arr3)           //will return new array with even nums
+
+
+
+
+
+
 
