954ca1f9ad4c9c2338ec7285928a3cabc61d4620
Harshita Nidhi <harshitanidhi17@gmail.com>
2021-10-18 15:03:42 +0530

    objects, functions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//scopes 
 let a = 40
 var b = 30
 
@@ -27,6 +27,54 @@ const print = () =>{
 }  
 print()                        
 // console.log(Name4)  //will give an error,in function global scope does not exists
+
+
+//this is an object and points to a parent object and right now parent object is this window
+// console.log(this)
+// console.log(this.alert('Hi'))
+
+
+//TO MAKE AN OBJECT
+let car = {
+    name:'c class',
+    manufacturer: 'Mercedes'
+}
+console.log(car)
+console.log(typeof car)
+// console.log(car.name)
+// console.log(car.manufacturer)
+
+//Function inside a object is called method
+let bus = {
+    name:'B class',
+    manufacturer: 'TATA',
+    print1:function(){
+        console.log(bus)
+    },
+    print2:function(a){
+        console.log(a)
+    }
+}
+bus.print1()
+bus.print2('ferrari')
+
+let car2 = {
+    name:'A class',
+    manufacturer: 'lamborgini',
+    print:function(){
+        // let manufacturer = 'BMW'
+        console.log(this)
+        console.log(manufacturer)
+        }
+}
+car2.print()
+
+
+
+
+
+
+
 
 
 
