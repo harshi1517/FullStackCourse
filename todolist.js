8fe93b90dac0bf92c80db5bc59240b911407d650
Harshita Nidhi <harshitanidhi17@gmail.com>
2021-10-18 11:52:04 +0530

    to avoid duplicates

--- todolist.js
+++ todolist.js
@@ -3,14 +3,51 @@ var input = document.querySelector('input')
 var list = document.querySelector('ul')
 console.log(button, input, list)
 
+// const callbackfun = (event) => {
+//     const inputValue = input.value
+//     const element = document.createElement('li')
+//     const textNode = document.createTextNode(inputValue)
+//     element.appendChild(textNode)
+//     list.appendChild(element)
+// }
+
+// const callbackfun = (event) => {
+//     const inputValue = input.value
+//     const element = document.createElement('li')
+//     const textNode = document.createTextNode(inputValue)
+//     element.appendChild(textNode)
+//     // element.setAttribute('id', '')       //it will give id to each element of the list
+//     list.appendChild(element)
+//     // element.addEventListener('click', () => {
+//     //     console.log('clicked')                //on clicking on the list element, it will show clicked in console     
+//     // })
+//     element.addEventListener('click', (e) => {
+//         console.log(e.target.innerHTML)          //on clicking on the list element, it will show that element in console 
+//     })
+// }
+
+//to avoid  duplicates 
+
+var chores = []
+
 const callbackfun = (event) => {
-    const inputValue = input.value
-    const element = document.createElement('li')
-    const textNode = document.createTextNode(inputValue)
-    element.appendChild(textNode)
-    list.appendChild(element)
+    const inputValue = input.value              //.value takes the input
+    if(chores.includes(inputValue)){
+        console.log('Already exists')
+    }
+    else{
+       chores.push(inputValue)
+       const element = document.createElement('li')
+       const textNode = document.createTextNode(inputValue)
+       element.appendChild(textNode)
+       list.appendChild(element)
+    //    element.addEventListener('click', (e) => {
+    //     console.log(e.target.innerHTML)  })       //on clicking on the list element, it will show that element in console 
+    } 
 }
 button.addEventListener('click', callbackfun)
+
+
 
 
 // var body = document.querySelector('body')
