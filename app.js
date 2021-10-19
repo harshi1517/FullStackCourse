457696c5aef166e212bac73a19388b868c0272d4
Harshita Nidhi <harshitanidhi17@gmail.com>
2021-10-19 15:33:55 +0530

    deleting, window prompt

--- app.js
+++ app.js
@@ -1,41 +1,31 @@
-console.log('one')    
-setTimeout( () => {console.log('timer 1')}, 2000)
-setTimeout( () => {console.log('timer 2')}, 1000)
-setTimeout( () => {console.log('timer 3')}, 3000)
-console.log('two')
+const list = document.querySelector('ul')
 
-//PROMISE
-let value = new Promise((resolve, reject) =>{
-    setTimeout(() => {
-        resolve('Hello')
-        // reject('Rejected')
-    }, 7000)
-})
-console.log(value)
-setTimeout(() => {console.log(value)}, 5000)
+//deleting element from veiwport
+var arr = [1, 2, 3, 4]
+arr.forEach((chore => {
+    const element = document.createElement('li')
+    const textNode = document.createTextNode(chore)
+    element.appendChild(textNode)
+    element.addEventListener('click', (e) => {
+        console.log(e.target.innerHTML)
+        const index = arr.indexOf(chore)
+        arr.splice(index, 1)     //will remore from array
+        e.target.remove()       //will remove from veiwport
+    })
+    list.appendChild(element)
+}))
 
-//fetch
-let data = fetch('https://jsonplaceholder.typicode.com/users')
-console.log(data)    //still we dont get the actual data
-setTimeout(() => {console.log(data)}, 2000)  
+arr.forEach((chore => {
+    const element = document.createElement('li')
+    const textNode = document.createTextNode(chore)
+    element.appendChild(textNode)
+    element.addEventListener('click', (e) => {
+        const userInput = window.prompt(`Are you sure you want to delete ${chore} ?`)
+        if(userInput == 'YES' || userInput == 'yes' || userInput == 'Yes')
+        e.target.remove()
+    })
+    list.appendChild(element)
+}))
 
-//.then
-let responce = fetch('https://jsonplaceholder.typicode.com/users').then(data => {
-    console.log(data)                
-})
 
-// let responce1 = fetch('https://jsonplaceholder.typicode.com/users').then(res => {
-//     let parsedData = res.json()
-//     console.log(parsedData)
-//     console.log(res)
-// })
 
-// let responce1 = fetch('https://jsonplaceholder.typicode.com/users').then(res => {
-//     let parsedData = res.json()
-//     console.log(parsedData)
-//     return(parsedData)   //we are returning it so that in next .then we have access to it
-// }).then(data => {console.log(data)})
-
-//we can also write the upper code(33-37) as
-let responce1 = fetch('https://jsonplaceholder.typicode.com/users').then(res => 
-    res.json()).then(data => {console.log(data)})
