2b743b54127852bc7a9bf896fbe67762532223d4
Harshita Nidhi <harshitanidhi17@gmail.com>
2021-10-19 23:09:05 +0530

    await

--- app.js
+++ app.js
@@ -52,8 +52,8 @@ val.then(val => {console.log(val)})
 let responce = fetch('https://jsonplaceholder.typicode.com/users')
    .then(res => res.json())
    .then(data => {
-       console.log(data)         //returning in form of array
-       data.forEach(user => {console.log(user)})   //returning in form of object  
+       console.log(data)     //returning in form of array
+       data.forEach(user => {console.log(user)}) //returning in form of object  
     })   
 
 //fetch and geting the data on veiwport in a table
@@ -69,24 +69,64 @@ let responce = fetch('https://jsonplaceholder.typicode.com/users')
 //         newRow.appendChild(id)
 //         table.appendChild(newRow)
 //         })
-//     })     //by this way we have to make the codes for name email userid also and it will become so lengthy so,
+//     })  // by this way we have to make the codes for name email userid also and 
+        //  it will become so lengthy so, we can use templates. 
 
-const table = document.querySelector('table')
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => {
-        data.forEach(user => {
-        let newRow = document.createElement('tr')
+// const table = document.querySelector('table')
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(data => {
+//         data.forEach(user => {
+//         let newRow = document.createElement('tr')
+//         let element = `<td>${user.id}</td>
+//             <td>${user.name}</td> 
+//             <td>${user.email}</td> 
+//             <td>${user.username}</td>  `
+//         newRow.innerHTML = element
+//         table.appendChild(newRow)
+//         })
+//     })
+
+//we can put the code in a function so that we can reuse it if needed
+const createRow = (user) => {
+    let newRow = document.createElement('tr')
         let element = `<td>${user.id}</td>
             <td>${user.name}</td> 
             <td>${user.email}</td> 
             <td>${user.username}</td>  `
         newRow.innerHTML = element
-        table.appendChild(newRow)
+        return newRow
+}
+const table = document.querySelector('table')
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(user => {
+        table.appendChild(createRow(user))
         })
     })
 
 
+//we can use await in place of promises
+//await is only valid in async function
+const fetchData = async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    console.log(data)
+}
+fetchData()
 
 
 
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const div = document.querySelector('div')
+
+const callbackfun = (event) => {
+    const inputvalue = input.value
+    const element = document.createElement('div')
+    const textnode = document.createTextNode(inputvalue)
+    element.appendChild(textnode)
+    div.appendChild(element)
+}
+button.addEventListener('click', callbackfun)
