e811e3a6a9570e8188290fd5bf1dad89ac075597
Harshita Nidhi <harshitanidhi17@gmail.com>
2021-10-20 12:45:00 +0530

    recipe cards

--- recipe.js
+++ recipe.js
@@ -1,40 +1,55 @@
+//RECIPE APP
 
 const input = document.querySelector('input')
 const button = document.querySelector('button')
-// const div = document.querySelector('div')
+const parent = document.querySelector('div.cards')
 
-// const callbackfun = (event) => {
-//     const inputvalue = input.value
-//     const element = document.createElement('div')
-//     const textnode = document.createTextNode(inputvalue)
-//     element.appendChild(textnode)
-//     div.appendChild(element)
+//making cards of the recipes
+//below code will only make one card  
+// const getData = async (food) => {
+//     const url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${food}`
+//     const res = await fetch(url)
+//     const data = await res.json()
+//     data.hits.forEach(recipe => {
+//         console.log(recipe)
+//         const template = `<div class="card">
+//         <img class="card__image" src="https://images.unsplash.com/photo-1481070414801-51fd732d7184?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80" alt="">
+//         <div class="card__content">
+//           <div class="card__header">
+//             <h4 class="card__title">Ham Sandwich</h4>
+//             <span class="card__price">$10.50</span>
+//           </div>
+//           <p class="card__text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+//         </div>
+//         </div>`
+//        //parent.appendChild(card)   //it will give an error
+//        parent.innerHTML = template  //will give only one card
+//     })
 // }
-// button.addEventListener('click', callbackfun)
 
-//RECIPE APP
-//to get the recipe of pizza on console
-const getData = async () => {
-    const url = 'https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=Pizza'
-    const res = await fetch(url)
-    const data = await res.json()
-    console.log(data)
-}
-button.addEventListener('click', () => {
-    getData()
-} )
-
-//to get the recipe of the input which we give in input text box 
-const getData1 = async (food) => {
+//cards for all recipes
+const getData = async (food) => {
     const url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${food}`
     const res = await fetch(url)
     const data = await res.json()
-    // console.log(data)
-    // console.log(data.hits)
-    data.hits.forEach(recipe => {
-        console.log(recipe)
+    data.hits.forEach(food => {
+        // console.log(food)
+        const template = `
+        <img class="card__image" src="https://images.unsplash.com/photo-1481070414801-51fd732d7184?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80" alt="">
+        <div class="card__content">
+          <div class="card__header">
+            <h4 class="card__title">Ham Sandwich</h4>
+            <span class="card__price">$10.50</span>
+          </div>
+          <p class="card__text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+        </div>`
+        const newCard = document.createElement('div.cards')
+        newCard.setAttribute('class', 'card')
+        newCard.innerHTML = template
+        console.log(newCard)
+        parent.appendChild(newCard)
     })
 }
 button.addEventListener('click', () => {
-    getData1(input.value)
+    getData(input.value)
 } )
